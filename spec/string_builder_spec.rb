0a5c198595c8e4cb93f3e590902595bf2921f3dc
Sabirah <sabirah.miah.ahammed@gmail.com>
2022-12-14 14:19:35 +0000

    added tests for StringBuilder

--- spec/string_builder_spec.rb
+++ spec/string_builder_spec.rb
@@ -5,9 +5,21 @@ RSpec.describe StringBuilder do
         string_builder = StringBuilder.new 
         expect(string_builder.output).to eq ""
     end
+
+    it "adds given strings" do
+        string_builder = StringBuilder.new
+        string_builder.add("Hello!")
+        expect(string_builder.output).to eq "Hello!"
+    end
+
+    it "checks string length" do
+        string_builder = StringBuilder.new
+        string_builder.add("Hello!")
+        expect(string_builder.size).to eq 6
+    end
 end
 
-#creates a new empty string
+#tests if it creates a new empty string
 #tests if added strings add
 #tests if the length returns for .size
 #tests if .output returns the given string
